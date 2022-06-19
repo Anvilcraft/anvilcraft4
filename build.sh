e415c338eb0c971b415ebe6a36e9459708b7331c
LordMZTE <lord@mzte.de>
2022-06-19 23:41:05 +0200

    init

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+VERSION="0.1.0"
+BUILDPATH="build/zip"
+
+rm -rf $BUILDPATH
+mkdir -p $BUILDPATH
+cp -r overrides $BUILDPATH/minecraft
+mkdir -p $BUILDPATH/minecraft/mods
+cp mmc-pack.json $BUILDPATH
+echo "InstanceType=OneSix" > $BUILDPATH/instance.cfg
+
+cd $BUILDPATH/minecraft/mods
+while read -r mod; do
+    echo "Downloading $mod"
+    curl -LO "$mod"
+done <../../../../mods.txt
+cd ../..
+
+rm ../*.zip
+zip -r "../ac4-$VERSION.zip" ./*

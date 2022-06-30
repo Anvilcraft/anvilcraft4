125472dbc0b94b2d7b8558b8c41a297142c791bf
LordMZTE <lord@mzte.de>
2022-06-30 16:15:13 +0200

    chore: remove shitty flight ring mod

--- overrides/kubejs/server_scripts/40-recipes.js
+++ overrides/kubejs/server_scripts/40-recipes.js
@@ -1,18 +1,4 @@
 function recipes(ev) {
-    ev.shaped(
-        "flight_rings:t_basic_ring_treasure",
-        [
-            "SWS",
-            "WRW",
-            "FWF",
-        ],
-        {
-            S: "terramine:mana_crystal",
-            W: "minecraft:feather",
-            R: "botania:pixie_ring",
-            F: "create:encased_fan",
-        }
-    );
 }
 
 onEvent("recipes", recipes);

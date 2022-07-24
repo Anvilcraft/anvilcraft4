38fe781c04f48781291d9023750b9a5ebe4f8557
LordMZTE <lord@mzte.de>
2022-07-24 10:57:44 +0200

    fix: recipe conflict with copper button

--- overrides/kubejs/server_scripts/40-recipes.js
+++ overrides/kubejs/server_scripts/40-recipes.js
@@ -1,27 +1,39 @@
 function recipes(ev) {
-	ev.remove({output: 'tempad:tempad'})
-	ev.shaped('tempad:tempad', [
-		'GGG',
-		'PCD',
-		'GGG'
+	ev.remove({output: "tempad:tempad"});
+
+    // conflicts with copper nuggets
+	ev.remove({output: "redstonebits:copper_button"});
+
+    ev.shaped("redstonebits:copper_button", [
+        "CC",
+        "CC"
+    ], {
+        C: "#c:copper_ingots"
+    });
+
+	ev.shaped("tempad:tempad", [
+		"GGG",
+		"PCD",
+		"GGG"
 	], {
-		G: 'minecraft:gold_ingot',
-		P: 'modern_industrialization:processing_unit',
-		C: 'powah:ender_core',
-		D: 'techreborn:digital_display'
-	})
-	ev.shaped('tempad:he_who_remains_tempad', [
-		'UTD',
-		'NSN',
-		'DRU'
+		G: "#c:gold_ingots",
+		P: "modern_industrialization:processing_unit",
+		C: "powah:ender_core",
+		D: "techreborn:digital_display"
+	});
+
+	ev.shaped("tempad:he_who_remains_tempad", [
+		"UTD",
+		"NSN",
+		"DRU"
 	], {
-		U: 'dimdoors:unravelled_block', 
-		N: 'minecraft:netherite_ingot',
-		D: 'minecraft:deepslate',
-		T: 'tempad:tempad',
-		R: 'blockus:legacy_nether_reactor_core',
-		S: 'minecraft:nether_star'
-	})
+		U: "dimdoors:unravelled_block", 
+        N: "#c:netherite_ingots",
+		D: "minecraft:deepslate",
+		T: "tempad:tempad",
+		R: "blockus:legacy_nether_reactor_core",
+		S: "minecraft:nether_star"
+	});
 }
 
 onEvent("recipes", recipes);

e1dbfd960eef24ae878782f47464ef15632fec17
LordMZTE <lord@mzte.de>
2022-07-24 13:12:23 +0200

    chore: 1.2.0 release

--- settings.zig
+++ settings.zig
@@ -1,5 +1,5 @@
 // Version number used for the archive name
-pub const version = "1.1.0";
+pub const version = "1.2.0";
 pub const build_dir = "build";
 pub const minecraft_version = "1.18.2";
 pub const fabric_loader_version = "0.14.8";
